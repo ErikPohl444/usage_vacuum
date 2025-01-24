d70db57c21a075c8775880446d49febce14fc0a0
Erik Pohl <epohl@dmigs.com>
2025-01-23 23:10:17 -0500

    upgraded logging process

--- src/setup_logging.py
+++ src/setup_logging.py
@@ -6,3 +6,14 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+def call_log(func):
+    def wrapper(*args, **kwargs):
+        try:
+            logger.info(f"Starting function {func.__name__}")
+            result = func(*args, **kwargs)
+            logger.info(f"Ending function {func.__name__}")
+            return result
+        except Exception as e:
+            logger.info(f"Exception {e} was caught in function {func.__name__}")
+    return wrapper
